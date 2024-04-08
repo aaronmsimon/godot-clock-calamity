c0fb81ed9bd4ef1306f6d43fa4e6bb617210b391
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-04-08 11:23:55 -0700

    Refactored Spawner & Set up Spawn Counter & Timer

--- Clock Calamity/Enemies/SpawnPoint.cs
+++ Clock Calamity/Enemies/SpawnPoint.cs
@@ -1,13 +1,59 @@
 using Godot;
+using Godot.Collections;
 
 public partial class SpawnPoint : Marker2D
 {
-    [Export] private AStarPathResource _path;
+    [Export] private AStarPathResource path;
+    [Export] private int enemiesToSpawn;
+    [Export] private float timeBetweenSpawns;
+    [Export] private PackedScene enemy;
+    [Export] private float speed;
+    [Export] private int health;
+    [Export] private int shots;
+    [Export] private float timeBetweenShots;
 
-    public AStarPathResource Path
+    private int enemyCount = 0;
+    private Timer timer = new Timer();
+
+    public override void _Ready()
     {
-        get {
-            return _path;
+        AddChild(timer);
+        timer.Timeout += OnTimerTimeout;
+
+        OnTimerTimeout();
+    }
+
+    private void SpawnEnemy()
+    {
+        // Increment enemies spawned counter
+        enemyCount++;
+        // Instantiate Enemy
+        Node2D enemyInstance = (Node2D)enemy.Instantiate();
+        // Set Position and Rotation
+        enemyInstance.GlobalPosition = GlobalPosition;
+        // Send Tilemap
+        TileMap tileMap = GetNode<TileMap>("%TileMap");
+        enemyInstance.Set("tileMap", tileMap);
+        // Add Paths
+        Array<Vector2I> paths = path.Cells;
+        enemyInstance.Set("paths", paths);
+        // Set other Public Variables
+        enemyInstance.Set("shots", shots);
+        enemyInstance.Set("timeBetweenShots", timeBetweenShots);
+        enemyInstance.Set("speed", speed);
+        enemyInstance.Set("health", health);
+        // Add to Tree
+        Node parent = GetNode<Node>("%Enemies");
+        parent.AddChild(enemyInstance);
+    }
+
+    private void OnTimerTimeout()
+    {
+        if (enemyCount < enemiesToSpawn)
+        {
+            SpawnEnemy();
+            // Start next timer
+            timer.Start(timeBetweenSpawns);
         }
     }
 }
--- Clock Calamity/Levels/AStarPathResource.cs
+++ Clock Calamity/Levels/AStarPathResource.cs
@@ -4,30 +4,12 @@ using Godot.Collections;
 public partial class AStarPathResource : Resource
 {
     [Export] private Array<Vector2I> _cells;
-    [Export] private int shots;
-    [Export] private float timeBetweenShots;
 
     public Array<Vector2I> Cells
     {
         get
         {
             return _cells;
-        }
-    }
-
-    public int Shots
-    {
-        get
-        {
-            return shots;
-        }
-    }
-
-    public float TimeBetweenShots
-    {
-        get
-        {
-            return timeBetweenShots;
         }
     }
 }
