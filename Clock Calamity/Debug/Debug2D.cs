215b860732882492d668daf5eb4d1c51f57e7d25
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-04-09 14:29:35 -0700

    Debug for A* Pathing

--- Clock Calamity/Debug/Debug2D.cs
+++ Clock Calamity/Debug/Debug2D.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 using Components.Pathfinding;
 
 namespace Debug.Shapes2D
@@ -12,18 +13,15 @@ namespace Debug.Shapes2D
         [Export] private bool drawGrid2DBarriers;
         [Export] private Color barrierColor = Colors.DarkRed;
         
-        // [ExportGroup("A* Pathing")]
-        // // [Export] private pathing component
-        // [Export] private bool drawPathing;
-        // [ExportSubgroup("Line Settings")]
-        // [Export] private float lineWidth;
-        // [Export] private Color lineColor = Colors.DarkGray;
-        // [Export] private Color startColor = Colors.RoyalBlue;
-        // [Export] private Color endColor = Colors.LightYellow;
-
-        public override void _Ready()
-        {
-        }
+        [ExportGroup("A* Pathing")]
+        [Export] Array<WaypointsResource> waypointsResource;
+        [Export] private bool drawPathing;
+        [ExportSubgroup("Line Settings")]
+        [Export] private float pathLineWidth = 2;
+        [Export] private Color pathLineColor = Colors.DarkGray;
+        [Export] private Color pathStartColor = Colors.LightGreen;
+        [Export] private Color pathMiddleColor = Colors.LightYellow;
+        [Export] private Color pathEndColor = Colors.LightPink;
 
         public override void _Process(double delta)
         {
@@ -33,6 +31,7 @@ namespace Debug.Shapes2D
         public override void _Draw()
         {
             DrawAStarGrid2DBarriers();
+            DrawAStarGrid2DPathing();
         }
 
         private void DrawAStarGrid2DBarriers()
@@ -55,9 +54,33 @@ namespace Debug.Shapes2D
             }
         }
 
-        // private void DrawAStarGrid2DPathing()
-        // {
+        private void DrawAStarGrid2DPathing()
+        {
+            if (waypointsResource == null) return;
+            if (astarGrid2DComponent == null) return;
+            if (astarGrid2DComponent.astarGrid2D == null || astarGrid2DComponent.tileMap == null) return;
+            if (waypointsResource.Count == 0) return;
+            if (!drawPathing) return;
 
-        // }
+            foreach (WaypointsResource waypoints in waypointsResource)
+            {
+                for (int i = 0; i < waypoints.waypoints.Count; i++)
+                {
+                    Vector2I waypoint = waypoints.waypoints[i];
+                    Color circleColor = pathMiddleColor;
+                    if (i == 0) circleColor = pathStartColor;
+                    if (i == waypoints.waypoints.Count - 1) circleColor = pathEndColor;
+
+                    DrawCircle(astarGrid2DComponent.tileMap.MapToLocal(waypoint) + astarGrid2DComponent.tileOffset * astarGrid2DComponent.tileMap.TileSet.TileSize, astarGrid2DComponent.tileMap.TileSet.TileSize.X / 4, circleColor);
+
+                    if (i > 0)
+                    {
+                        Vector2 from = astarGrid2DComponent.tileMap.MapToLocal(waypoints.waypoints[i - 1]) + astarGrid2DComponent.tileOffset * astarGrid2DComponent.tileMap.TileSet.TileSize;
+                        Vector2 to = astarGrid2DComponent.tileMap.MapToLocal(waypoints.waypoints[i]) + astarGrid2DComponent.tileOffset * astarGrid2DComponent.tileMap.TileSet.TileSize;
+                        DrawLine(from, to, pathLineColor, pathLineWidth);
+                    }
+                }
+            }
+        }
     }
 }
