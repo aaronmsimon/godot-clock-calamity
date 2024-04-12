14b6199188934b41c2887bdd882a13f9efdc28e2
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-04-11 19:19:57 -0700

    Resource naming convention

--- Clock Calamity/Components/Game/GameStatsResource.cs
+++ Clock Calamity/Components/Game/GameStatsResource.cs
@@ -2,7 +2,7 @@ using Godot;
 
 namespace Components.Game
 {
-    public partial class GameStats : Resource
+    public partial class GameStatsResource : Resource
     {
         [Export] public int score { get; set; } = 0;
         [Export] public int shotsFired { get; set; } = 0;
