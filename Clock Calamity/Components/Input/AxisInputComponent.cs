c88ba18180ad5da26dc8c7e3fe2559b87ff1770a
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-04-14 11:26:07 -0700

    Adding safety features to move input and move components

--- Clock Calamity/Components/Input/AxisInputComponent.cs
+++ Clock Calamity/Components/Input/AxisInputComponent.cs
@@ -10,8 +10,19 @@ namespace Components.Inputs
 
         public float InputAxis { get; private set; }
 
+        public override void _Ready()
+        {
+            if (negativeAction == "" || positiveAction == "")
+            {
+                GD.PrintErr("The AxisInputComponent " + this.Name + " requires both a Negative and Positive Action. Please make sure both are configured on node " + this.GetPath());
+                return;
+            }
+        }
+
         public override void _Input(InputEvent @event)
         {
+            if (negativeAction == "" || positiveAction == "") return;
+
             InputAxis = Input.GetAxis(negativeAction, positiveAction);
         }
     }
--- Clock Calamity/Components/Movement/FixedMovement2DComponent.cs
+++ Clock Calamity/Components/Movement/FixedMovement2DComponent.cs
@@ -7,8 +7,19 @@ namespace Components.Movement
     {
         [Export] private Node2D actor;
 
+        public override void _Ready()
+        {
+            if (actor == null)
+            {
+                GD.PrintErr("The FixedMovement2DComponent " + this.Name + " requires a Node2D to move. Please add one to node " + this.GetPath());
+                return;
+            }
+        }
+        
         public void MoveActorToMarker(Marker2D marker2D)
         {
+            if (actor == null || marker2D == null) return;
+            
             actor.GlobalPosition = marker2D.GlobalPosition;
         }
     }
